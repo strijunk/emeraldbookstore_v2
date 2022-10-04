3c740972dcd1efe97208957ca07034d030f8673f
strijunk <izzulkhaq75@gmail.com>
2022-10-04 13:18:31 +0700

    reupload files in a proper way

---
+++ helper/CONSTANT.php
@@ -0,0 +1,12 @@
+<?php
+
+// connection
+define("HOST", "localhost");
+define("UNAME", "root");
+define("PASS", "");
+define("DB", "emeraldbookstore");
+
+// tables
+// define("TB_BARANG", "tb_barang");
+
+?>
